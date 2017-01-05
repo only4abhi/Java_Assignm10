9f6b91ad5323cbebfe5cb170a078c2eed7c88358
only4abhi <abhishekchanda.mailbox@gmail.com>
2017-01-05 17:43:54 +0530

    Add files via upload

---
+++ BooleanString.java
@@ -0,0 +1,13 @@
+
+public class BooleanString {
+
+    public static void main(String[] args) {
+    	
+    	String S1="New Learn";
+    	String S2="Good Learn";
+        boolean b3 = (S1 == S2) ? true:false;
+        System.out.println("Value of boolean variable b3 is :" + b3);            
+
+
+    }
+}
